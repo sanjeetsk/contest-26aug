9edcf803e9251c15952e25b99da13953fd955006
sanjeetsk <ssanjeetkumar67@gmail.com>
2023-08-26 23:43:27 +0530

    first change

--- src/Component/BookData/BookDetail.js
+++ src/Component/BookData/BookDetail.js
@@ -11,32 +11,13 @@ const BookDetail = ({ books}) => {
     // Find the selected book by its ID
     const selectedBook = books.find((book) => book.id === id);
 
-
-    // let thumbnail = book.volumeInfo.imageLinks && book.volumeInfo.imageLinks.smallThumbnail
-
     return (
-        // <div className='overlay'>
-        //     <div className='overlay-inner'>
-        //         <button className='close' onClick={onClose}><i className="fa-regular fa-circle-xmark"></i></button>
-        //         <div className='inner-box'>
-        //             <img src={thumbnail} alt='book'></img>
-        //             <div className='info'>
-        //                 <h1>{book.volumeInfo.title}</h1>
-        //                 <h3>{book.volumeInfo.authors}</h3>
-        //                 <h4>{book.volumeInfo.publisher}<span>{book.volumeInfo.publishedDate}</span></h4>
-        //                 <a href={book.volumeInfo.previewLink}><button>More Info!</button></a>
-        //             </div>
-        //         </div>
-        //         <h4 className='description'>{book.volumeInfo.description}</h4>
-        //     </div>
-        // </div>
         <div>
-            <h2>Book Detail Page</h2>
             {selectedBook ? (
                 <div>
                     <h3>{selectedBook.volumeInfo.title}</h3>
                     <p>{selectedBook.volumeInfo.description}</p>
-                    <img src={selectedBook.volumeInfo.imageLinks.smallThumbnail} />
+                    <img src={selectedBook.volumeInfo.imageLinks.smallThumbnail} alt='book' />
                 </div>
             ) : (
                 <p>Book not found</p>
--- src/Component/BookData/Books.js
+++ src/Component/BookData/Books.js
@@ -1,20 +1,7 @@
 
-import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Books = ({ books }) => {
-  const [book, setBook] = useState([]);
-  const [selectedBook, setSelectedBook] = useState(false);
-
-  const handleBookClick = (item) => {
-    setSelectedBook(true);
-    setBook(item);
-  };
-
-  const handleClose = () => {
-    setSelectedBook(false);
-    setBook([]);
-  };
 
   return (
     <>
@@ -23,7 +10,7 @@ const Books = ({ books }) => {
           let thumbnail = item.volumeInfo.imageLinks && item.volumeInfo.imageLinks.smallThumbnail;
           if (thumbnail !== undefined) {
             return (
-              <div className="card" key={index} onClick={() => handleBookClick(item)}>
+              <div className="card" key={index}>
                 <Link to={`/book/${item.id}`}>
                   <img src={thumbnail} alt="books" />
                   <div className="bottom">
